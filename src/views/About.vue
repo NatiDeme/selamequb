a411ba635ba3f295650946e461b67f5b90be2f1a
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-01-03 15:43:16 +0300

    progress update with routing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
+    <SideNav />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import SideNav from "../components/SideNav.vue";
+
+export default {
+  name: 'About',
+  components: {
+    SideNav
+  }
+}
+
+</script>
+

364c405de51caaaf7c568f41159290ed1ab48f66
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-02-01 12:27:51 +0300

    login page ready

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,13 +17,14 @@
           placeholder="Password"
           class="w-80 h-14 pl-3 rounded-md"
         />
-
-        <button
-          type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <router-link to="/">
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button></router-link
         >
-          Login
-        </button>
       </form>
     </div>
   </div>
